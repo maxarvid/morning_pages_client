0217a24b9ce444b75e2728fe7029ee030f03c6ca
maxarvid <max.arvid.anderson@gmail.com>
2022-06-15 11:36:41 +0200

    🔒️ adds auth slice to dispatch current user state

--- cypress/e2e/userCanSeeListOfMorningPages.cy.js
+++ cypress/e2e/userCanSeeListOfMorningPages.cy.js
@@ -1,14 +1,23 @@
 describe("When user navigates to their Morning Pages", () => {
   beforeEach(() => {
-    cy.intercept("GET", "**/themes", { fixture: "themesResponse.json" }); 
+    cy.intercept("GET", "**/themes", { fixture: "themesResponse.json" });
     cy.intercept("GET", "**/morning_pages", {
       fixture: "morningPagesIndex.json",
     }).as("getMorningPages");
     cy.visit("/");
     cy.window()
       .its("store")
-      .invoke("dispatch", { type: "SET_CURRENT_USER", payload: true });
+      .invoke("dispatch", { type: "auth/setCurrentUser", payload: true });
     cy.get("[data-cy=morning-pages-btn]").click();
+  });
+
+  it("is expected to store the current user status in application state", () => {
+    cy.window()
+      .its("store")
+      .invoke("getState")
+      .its("auth")
+      .its("currentUser")
+      .should("eq", true);
   });
 
   it("is expected to display an index of morning pages", () => {
--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import morningPagesSlice from "./features/morningPagesSlice";
 import themesSlice from "./features/themesSlice";
+import authSlice from "./features/authSlice";
 
 export const store = configureStore({
   reducer: {
     morningPages: morningPagesSlice,
     themes: themesSlice,
+    auth: authSlice,
   },
 });
