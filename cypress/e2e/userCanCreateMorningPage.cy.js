62e709f04a917ca30f3389f1b19cfe3dcbb67652
maxarvid <max.arvid.anderson@gmail.com>
2022-06-15 15:48:24 +0200

    :test_tube: adds failing morning page create spec

---
+++ cypress/e2e/userCanCreateMorningPage.cy.js
@@ -0,0 +1,36 @@
+describe("When user creates a new morning page", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/themes", { fixture: "themesResponse.json" });
+    cy.intercept("GET", "**/morning_pages", {
+      fixture: "morningPagesIndex.json",
+    }).as("getMorningPages");
+    cy.visit("/");
+    cy.window()
+      .its("store")
+      .invoke("dispatch", { type: "auth/setCurrentUser", payload: true });
+    cy.get("[data-cy=morning-pages-btn]").click();
+  });
+
+  describe("successfully", () => {
+    beforeEach(() => {
+      cy.intercept("POST", "**/morning_pages/**", {
+        fixture: "morningPageCreate.json",
+      }).as("morningPageCreate");
+      cy.get("[data-cy=morning-page-new-btn").click()
+      cy.get("[data-cy=morning-page-title-input]").type("This is the title");
+      cy.get("[data-cy=morning-page-body-input]").type("this is the body");
+      cy.get("[data-cy=morning-page-submit-btn]").click();
+    });
+
+    it("is expected to make an API call", () => {
+      cy.wait("@morningPageCreate").its("request.method").should("eql", "POST");
+    });
+
+    it("is expected to respond with a message", () => {
+      cy.get("[data-cy=morning-page-toast]").should(
+        "containt.text",
+        "Your morning page was saved!"
+      );
+    });
+  });
+});
