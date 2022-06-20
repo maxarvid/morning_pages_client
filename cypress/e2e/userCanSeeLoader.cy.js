2b6d8c22565a359b44edb875fac5df1fd9338443
maxarvid <max.arvid.anderson@gmail.com>
2022-06-20 21:18:54 +0200

    🥳 refactors loader spec to use req, res intercept arguments

--- cypress/e2e/userCanSeeLoader.cy.js
+++ cypress/e2e/userCanSeeLoader.cy.js
@@ -3,34 +3,16 @@ describe("When authenticating, before the server responds", () => {
     cy.visitThemes();
   });
 
-  describe("successfully", () => {
-    beforeEach(() => {
-      cy.intercept("POST", "**/auth/sign_in", {
-        fixture: "signInResponse.json",
-        delay: 500,
-      }).as("signIn");
-      cy.signIn();
-    });
-
-    it("is expected to display a loader while logging user in", () => {
-      cy.wait("@signIn");
-      cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
-    });
-  });
-
-  describe("unsuccessfully", () => {
-    beforeEach(() => {
-      cy.intercept("POST", "**/auth/sign_in", {
-        statusCode: 401,
-        fixture: "signInFailure.json",
-        delay: 4000,
-      }).as("signInFailure");
-      cy.signIn();
-    });
-
-    it("is expected to display a loader while logging user in", () => {
-      cy.wait("@signInFailure");
-      cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
-    });
+  it("is expected to display a loader while logging user in", () => {
+    let resolveReq;
+    cy.intercept("POST", "**/auth/sign_in", (req) => {
+      return new Cypress.Promise((resolve) => {
+        resolveReq = resolve;
+      }).then(req.reply);
+    }).as("signIn");
+    cy.signIn();
+    cy.get("[data-cy=loader-container]")
+      .should("contain.text", "Loading")
+      .then(() => resolveReq());
   });
 });
