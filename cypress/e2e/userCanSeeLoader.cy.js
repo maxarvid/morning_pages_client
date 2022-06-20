a307c9879eb7e12ff2c839d406b35ea7464014e6
maxarvid <max.arvid.anderson@gmail.com>
2022-06-20 21:03:12 +0200

    ⏲  adds more time to intercept delay

--- cypress/e2e/userCanSeeLoader.cy.js
+++ cypress/e2e/userCanSeeLoader.cy.js
@@ -7,18 +7,15 @@ describe("When authenticating, before the server responds", () => {
     beforeEach(() => {
       cy.intercept("POST", "**/auth/sign_in", {
         fixture: "signInResponse.json",
-        delay: 2000,
+        delay: 500,
       }).as("signIn");
-      cy.intercept("GET", "**/auth/validate_token", {
-        fixture: "signInResponse.json",
-        delay: 2000,
-      });
       cy.signIn();
-      cy.wait("@signIn");
     });
 
     it("is expected to display a loader while logging user in", () => {
-      cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
+      cy.wait("@signIn").then(() => {
+        cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
+      });
     });
   });
 
@@ -27,19 +24,15 @@ describe("When authenticating, before the server responds", () => {
       cy.intercept("POST", "**/auth/sign_in", {
         statusCode: 401,
         fixture: "signInFailure.json",
-        delay: 2000,
+        delay: 4000,
       }).as("signInFailure");
-      cy.intercept("GET", "**/auth/validate_token**", {
-        statusCode: 401,
-        fixture: "signInFailure.json",
-        delay: 2000,
-      });
       cy.signIn();
-      cy.wait("@signInFailure");
     });
 
     it("is expected to display a loader while logging user in", () => {
-      cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
+      cy.wait("@signInFailure").then(() => {
+        cy.get("[data-cy=loader-container]").should("contain.text", "Loading");
+      });
     });
   });
 });
