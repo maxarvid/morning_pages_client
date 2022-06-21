3da71534d3b1062b1ca1de0fad635d9449d12caa
maxarvid <max.arvid.anderson@gmail.com>
2022-06-21 11:11:58 +0200

    🛂 adds headers to morning page crud actions

--- src/modules/MorningPagesService.js
+++ src/modules/MorningPagesService.js
@@ -7,9 +7,17 @@ import {
 import { store } from "../state/store";
 
 const MorningPagesService = {
+  async getHeaders() {
+    const headers = await JSON.parse(
+      localStorage.getItem("J-tockAuth-Storage")
+    );
+    return headers;
+  },
   async index() {
     try {
-      const { data } = await axios.get("/morning_pages");
+      const { data } = await axios.get("/morning_pages", {
+        headers: this.getHeaders(),
+      });
       store.dispatch(setMorningPages(data.morning_pages));
     } catch (error) {
       toast.error("Something went wrong, try again later");
@@ -17,7 +25,9 @@ const MorningPagesService = {
   },
   async show(id) {
     try {
-      const { data } = await axios.get(`/morning_pages/${id}`);
+      const { data } = await axios.get(`/morning_pages/${id}`, {
+        headers: this.getHeaders(),
+      });
       store.dispatch(setMorningPage(data.morning_page));
     } catch (error) {
       toast.error("Something went wrong, try again later");
@@ -25,7 +35,11 @@ const MorningPagesService = {
   },
   async create(props) {
     try {
-      const { data } = await axios.post(`themes/${props.themeId}/morning_pages`, props);
+      const { data } = await axios.post(
+        `themes/${props.themeId}/morning_pages`,
+        props,
+        { headers: this.getHeaders }
+      );
       toast(data.message);
     } catch (error) {
       toast.error("Something went wrong, try again later");
